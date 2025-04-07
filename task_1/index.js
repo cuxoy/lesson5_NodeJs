1bd49cde4fc2615df712087aba48420ece19bc01
Dmytro <ccccuxoy@gmail.com>
2025-04-07 17:17:41 +0300

    error checkup added to task 1,3

--- task_1/index.js
+++ task_1/index.js
@@ -1,5 +1,17 @@
 const fs = require("fs");
 
 fs.readFile("./source.txt", "utf-8", (error, data) => {
-  fs.writeFile("./copy.txt", data, () => {});
+  if (error) {
+    console.log(`Reading error: ${error}`);
+    return;
+  }
+
+  fs.writeFile("./copy.txt", data, (error) => {
+    if (error) {
+      console.log(`Writing error: ${error}`);
+      return;
+    } else {
+      console.log("file successfully created!");
+    }
+  });
 });
--- task_3/index.js
+++ task_3/index.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 
 fs.readFile("./source.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(`Reading error: ${error}`);
+    return;
+  }
   const replaced = data.replace(/Node/g, "NODE.JS");
-  fs.writeFile("./copy.txt", replaced, () => {});
+
+  fs.writeFile("./copy.txt", replaced, (error) => {
+    if (error) {
+      console.log(`Writing error: ${error}`);
+      return;
+    } else {
+      console.log("file successfully created!");
+    }
+  });
 });
